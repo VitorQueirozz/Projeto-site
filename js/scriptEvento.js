aa449b3d627286b334b3390b62dd9d3226cdce89
Vitor Manoel <130392470+VitorWAS@users.noreply.github.com>
2023-09-11 17:36:52 -0300

    position no body

--- js/scriptEvento.js
+++ js/scriptEvento.js
@@ -2,15 +2,18 @@
 const imagensAmpliaveis = document.querySelectorAll('.minha-imagem');
 const imagemAmpliada = document.querySelector('#imagem-ampliada');
 const imagemAmpliadaConteudo = imagemAmpliada.querySelector('img');
+const bodyEventos = document.querySelector('body');
 
 imagensAmpliaveis.forEach(imagem => {
     imagem.addEventListener('click', () => {
         const src = imagem.getAttribute('src');
         imagemAmpliadaConteudo.setAttribute('src', src);
         imagemAmpliada.style.display = 'block';
+        bodyEventos.style.position = 'fixed'
     });
 });
 
 imagemAmpliada.addEventListener('click', () => {
     imagemAmpliada.style.display = 'none';
+    bodyEventos.style.position = 'relative'
 });
